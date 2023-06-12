f4e5455a57ddf9301f58fd10ebc2d220c57a9828
CodingStrqf <l.ruy42@orange.fr>
2023-06-12 19:52:13 +0200

    test add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('API mspr')
+    .setDescription('API for some data of the mspr')
+    .setVersion('1.0')
+    .addTag('Your API Tag')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
