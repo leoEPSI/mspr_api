39405835b56429e5788b67e307aa4ba3e74e643d
CodingStrqf <l.ruy42@orange.fr>
2023-07-02 15:06:12 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  const corsOptions: CorsOptions = {
+    origin: true, // Permettre toutes les origines
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Configurer les méthodes HTTP autorisées
+    allowedHeaders: ['Content-Type', 'Authorization'], // Configurer les en-têtes autorisés
+    preflightContinue: false, // Désactiver les requêtes OPTIONS de pré-vérification
+    optionsSuccessStatus: 204, // Retourner le statut 204 pour les réponses OPTIONS
+  };
+  app.enableCors(corsOptions);
+
   const config = new DocumentBuilder()
     .setTitle('API mspr')
     .setDescription('API for some data of the mspr')
@@ -16,6 +26,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
   
-  await app.listen(3000);
+  await app.listen(8081);
 }
 bootstrap();
